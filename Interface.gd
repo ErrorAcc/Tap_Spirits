d534708a25de19aa836bf38b60ac8c0d1253b520
ErrorAcc <raphael.rueck@web.de>
2020-10-05 17:17:22 +0200

    Create Interface.gd

---
+++ Interface.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var coinscore = 0
+func _ready():
+	print("testtttttttttttttttttttttttttttttttttt")
+	pass 
+
+func get_coins():
+	#get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+	pass
