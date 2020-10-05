c013eaf5a09c596a4caabf8d6ab002400ba7451c
ErrorAcc <raphael.rueck@web.de>
2020-10-05 18:29:11 +0200

    Update Interface.gd

--- Interface.gd
+++ Interface.gd
@@ -1,10 +1,22 @@
 extends Node
 
-var coinscore = 0
+
+var coins = 0
+
+onready var health = utils.get_main_node().get_node("HealthBarTest").get_node("Health")
+onready var gegner = utils.get_main_node().get_node("HealthBarTest").get_node("Health").get_node("Gegner")
+
 func _ready():
-	print("testtttttttttttttttttttttttttttttttttt")
+	gegner.connect("change_frame",self,"get_coins")
+	get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+	get_node("Lvl_Counter").set_text(str(Gamee.lvlscore))
 	pass 
 
 func get_coins():
-	#get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+
+	Gamee.lvlscore += 1
+	Gamee.coinscore += 1
+	get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+	
+	get_node("Lvl_Counter").set_text(str(Gamee.lvlscore))
 	pass
