d7ca1ac9b7ad6b2c10907606773b74abe1b2ebc0
ErrorAcc <raphael.rueck@web.de>
2020-10-05 18:28:56 +0200

    Update Gamee.gd

--- Gamee.gd
+++ Gamee.gd
@@ -2,13 +2,16 @@ extends Node
 
 var bestscore = 0 setget set_bestscore
 var coinscore = 0 setget set_coinscore
+var lvlscore = 0 setget set_lvlscore
 
 const filepath_bs = "user://bestscore.data"
 const filepath_cs = "user://coinscore.data"
+const filepath_ls = "user://lvlscore.data"
 
 func _ready():
 	load_bestscore()
 	load_coinscore()
+	load_lvlscore()
 	pass
 
 func load_bestscore():
@@ -50,4 +53,25 @@ func save_coinscore():
 func set_coinscore(new_value):
 	coinscore = new_value
 	save_coinscore()
+	pass
+
+
+func load_lvlscore():
+	var file = File.new()
+	if not file.file_exists(filepath_ls): return
+	file.open(filepath_ls, File.READ)
+	lvlscore = file.get_var()
+	file.close()
+	pass
+
+func save_lvlscore():
+	var file = File.new()
+	file.open(filepath_ls, File.WRITE)
+	file.store_var(lvlscore)
+	file.close()
+	pass
+
+func set_lvlscore(new_value):
+	lvlscore = new_value
+	save_lvlscore()
 	pass
