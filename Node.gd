e8017c827e88f27716b30166f2d71580cd54988b
ErrorAcc <raphael.rueck@web.de>
2020-10-07 16:59:45 +0200

    Update Node.gd

--- Node.gd
+++ Node.gd
@@ -1,12 +1,6 @@
-# script: main Node
 extends Node
 
 
 
 func _ready():
-	#var player_health = $Gegner
-	#var healthbar = $HealthBarTest
-	#player_health.connect("changed", $HealthBarTest, "set_value")
-	#player_health.connect("max_changed", $HealthBarTest, "set_max")
-	#player_health.initialize()
 	pass
