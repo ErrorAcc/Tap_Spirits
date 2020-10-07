55eb4589bfd975175ced7fa4de0eca713884a5f9
ErrorAcc <raphael.rueck@web.de>
2020-10-07 16:59:54 +0200

    Update Interface.gd

--- Interface.gd
+++ Interface.gd
@@ -5,8 +5,10 @@ var coins = 0
 
 onready var health = utils.get_main_node().get_node("HealthBarTest").get_node("Health")
 onready var gegner = utils.get_main_node().get_node("HealthBarTest").get_node("Health").get_node("Gegner")
+onready var upgrades = utils.get_main_node().get_node("Upgrades")
 
 func _ready():
+	upgrades.connect("dmgUg", self, "_dmg_per_click")
 	gegner.connect("change_frame",self,"get_coins")
 	get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	get_node("Lvl_Counter").set_text(str(Gamee.lvlscore))
@@ -19,4 +21,9 @@ func get_coins():
 	get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	
 	get_node("Lvl_Counter").set_text(str(Gamee.lvlscore))
+	pass
+
+func _dmg_per_click():
+	print(health.schaden)
+	get_node("dmg_click").set_text(str(health.schaden))
 	pass
