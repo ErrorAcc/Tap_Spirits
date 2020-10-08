bc53e8890699dd4430bc37820f8040774805a922
ErrorAcc <raphael.rueck@web.de>
2020-10-08 17:38:05 +0200

    Update Upgrades.gd

--- Upgrades.gd
+++ Upgrades.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+export var crit_chance = 0
+export var crit_damge = 1
+
+signal critchance
+signal critdamage
 signal dmgUg
 
 func _ready():
@@ -7,6 +12,17 @@ func _ready():
 
 
 func _on_DamageButton_pressed():
-	print("dmgUg")
 	emit_signal("dmgUg")
 	pass
+
+
+func _on_Crit_Chance_pressed():
+	crit_chance = crit_chance + 1
+	emit_signal("critchance")
+	pass 
+
+
+func _on_Crit_Damage_pressed():
+	crit_damge = crit_damge + 0.1
+	emit_signal("critdamage")
+	pass 
