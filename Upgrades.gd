fb9d19d15dc38a575304556cc267d079bdedcb6a
ErrorAcc <raphael.rueck@web.de>
2020-10-10 17:12:30 +0200

    Update Upgrades.gd

--- Upgrades.gd
+++ Upgrades.gd
@@ -3,6 +3,11 @@ extends Node2D
 export var crit_chance = 0
 export var crit_damge = 1
 
+export var coins = 0
+export var dmg_cost = 5
+export var crit_chance_cost = 1
+export var crit_dmg_cost = 2
+
 signal critchance
 signal critdamage
 signal dmgUg
@@ -12,17 +17,26 @@ func _ready():
 
 
 func _on_DamageButton_pressed():
-	emit_signal("dmgUg")
+	if Gamee.coinscore >= dmg_cost:
+		Gamee.coinscore -= dmg_cost
+		emit_signal("dmgUg")
+		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass
 
 
 func _on_Crit_Chance_pressed():
-	crit_chance = crit_chance + 1
-	emit_signal("critchance")
+	if Gamee.coinscore >= crit_chance_cost:
+		Gamee.coinscore -= crit_chance_cost
+		crit_chance = crit_chance + 1
+		emit_signal("critchance")
+		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass 
 
 
 func _on_Crit_Damage_pressed():
-	crit_damge = crit_damge + 0.1
-	emit_signal("critdamage")
+	if Gamee.coinscore >= crit_dmg_cost:
+		Gamee.coinscore -= crit_dmg_cost
+		crit_damge = crit_damge + 0.1
+		emit_signal("critdamage")
+		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass 
