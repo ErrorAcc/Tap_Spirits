fe23d6e9974c275dea2275938978019bfede96f7
ErrorAcc <raphael.rueck@web.de>
2020-11-06 23:09:10 +0100

    Update Upgrades.gd

--- Upgrades.gd
+++ Upgrades.gd
@@ -8,6 +8,9 @@ export var dmg_cost = 5
 export var crit_chance_cost = 1
 export var crit_dmg_cost = 2
 
+onready var interface = utils.get_main_node().get_node("Interface")
+onready var health = utils.get_main_node().get_node("HealthBarTest").get_node("Health")
+
 signal critchance
 signal critdamage
 signal dmgUg
@@ -20,7 +23,7 @@ func _on_DamageButton_pressed():
 	if Gamee.coinscore >= dmg_cost:
 		Gamee.coinscore -= dmg_cost
 		emit_signal("dmgUg")
-		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+		interface.get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass
 
 
@@ -29,7 +32,7 @@ func _on_Crit_Chance_pressed():
 		Gamee.coinscore -= crit_chance_cost
 		crit_chance = crit_chance + 1
 		emit_signal("critchance")
-		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+		interface.get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass 
 
 
@@ -38,5 +41,25 @@ func _on_Crit_Damage_pressed():
 		Gamee.coinscore -= crit_dmg_cost
 		crit_damge = crit_damge + 0.1
 		emit_signal("critdamage")
-		utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+		interface.get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+	pass 
+
+
+func _on_reset_pressed():
+	health.schaden = 1
+	crit_damge = 0
+	crit_chance = 0
+	Gamee.coinscore = 0
+	Gamee.lvlscore = 0
+	interface.get_node("Coin_Counter").set_text(str(Gamee.coinscore))
+	interface.get_node("Lvl_Counter").set_text(str(Gamee.lvlscore))
+	interface.get_node("crit_dmg").set_text(str(crit_damge))
+	interface.get_node("dmg_click").set_text(str(health.schaden))
+	interface.get_node("crit_chance").set_text(str(crit_chance))
+	pass
+
+
+func _on_Gold_pressed():
+	Gamee.coinscore += 100
+	utils.get_main_node().get_node("Interface").get_node("Coin_Counter").set_text(str(Gamee.coinscore))
 	pass 
